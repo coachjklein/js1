0f37612767ef1b802c44b3230d37d8991ec253fc
joshk <coachjklein@yahoo.com>
2018-05-30 18:44:07 -0700

    takehome2

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,48 @@ var myName = "Joshua";
 console.log(myName);
 var hobby = "I enjoy sports and spending time with my wife and kids.";
 console.log(hobby);
+let birthday = new Date("7/9/81");
+const goal = "To learn enough that I can make money doing this";
+console.log(myName, birthday, goal);
+
+
+/*
+1. Declare 2 let variables:
+  a favorite movie
+  b favorite favorite
+2. declare 2 const variables:
+  a Year movie came out
+  b ingredients of your favorite food as an array
+3. alert your favorite food with its ingredients
+4 confirm the movie and year
+*/
+
+/*let favoriteMovie = "The Shack";
+let favoriteFood = "spaghetti";
+const movieYear = new Date(06/01/2017);
+const ingredients = ["marinara", "noodles"];
+alert(favoriteFood + ingredients);
+confirm(favoriteMovie + movieYear);*/
+
+var userFirstName = "What is your first name?";
+var userLastName = "What is your last name?";
+console.log(userFirstName);
+alert(userLastName);
+var userBirthday = new Date(userBirthday);
+prompt(userBirthday);
+alert(userBirthday);
+
+/*Day 3 homework
+let favColor = Prompt ('give me your fav color', '...')
+
+switch(favColor.upperLowerCase()) {
+  case 'blue':
+    alert('${favColor} is my favorite too!');
+    NEXT LINE IS EXAMPLE IN ES 5, ABOVE IS IN ES 6
+    alert('Hey, '+name+' that color ('+favColor+') is awesome!')
+    break;
+    DO THE OTHER CASES YOU WANT HERE EX GREEN RED ORANGE
+  default:
+    alert('Hey ${name}, thats not a valid option.');
+    break;
+}
